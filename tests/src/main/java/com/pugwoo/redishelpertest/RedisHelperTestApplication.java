33594f8823cee6c1f98f7aed42573bfae475194b
pugwoo <pugwoo@gmail.com>
2022-08-27 19:30:20 +0800

    调整目录结构

---
+++ tests/src/main/java/com/pugwoo/redishelpertest/RedisHelperTestApplication.java
@@ -0,0 +1,13 @@
+package com.pugwoo.redishelpertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisHelperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisHelperTestApplication.class, args);
+	}
+
+}
---
+++ tests/src/test/java/com/pugwoo/redishelpertest/RedisHelperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pugwoo.redishelpertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisHelperTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
