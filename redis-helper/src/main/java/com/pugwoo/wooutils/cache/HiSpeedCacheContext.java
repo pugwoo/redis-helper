c5dfbf219cfd718af774bf7e5b761093def06119
pugwoo <pugwoo@gmail.com>
2023-06-13 22:16:42 +0800

    增加通过HiSpeedContext控制尝试进行强制刷新，但如果失败了就走缓存

--- redis-helper/src/main/java/com/pugwoo/wooutils/cache/HiSpeedCacheContext.java
+++ redis-helper/src/main/java/com/pugwoo/wooutils/cache/HiSpeedCacheContext.java
@@ -12,9 +12,21 @@ public class HiSpeedCacheContext {
     private static final ThreadLocal<Boolean> forceRefreshOnce = new ThreadLocal<>();
 
     /**
+     * 尝试强制刷新缓存，如果刷新失败则走原来缓存，如果刷新成功则更新缓存。
+     */
+    private static final ThreadLocal<Boolean> tryForceRefreshOnce = new ThreadLocal<>();
+
+    /**
      * 停止本次查询的缓存，即不走缓存，也不更新缓存
      */
     private static final ThreadLocal<Boolean> disableOnce = new ThreadLocal<>();
+
+    /**
+     * 尝试强制刷新缓存，如果刷新失败则走原来缓存，如果刷新成功则更新缓存。这个设置只生效一次。
+     */
+    public static void tryForceRefreshOnce() {
+        tryForceRefreshOnce.set(true);
+    }
 
     /**
      * 设置本次缓存走强制刷新，每次设置不管最终调用成功还是失败，都只强制刷新一次，如需要继续强制刷新，需要再次设置。
@@ -29,6 +41,16 @@ public class HiSpeedCacheContext {
      */
     public static void disableOnce() {
         disableOnce.set(true);
+    }
+
+    protected static boolean getTryForceRefresh() {
+        Boolean b = tryForceRefreshOnce.get();
+        if (b != null && b) {
+            tryForceRefreshOnce.set(false); // 拿一次就清空
+            return true;
+        } else {
+            return false;
+        }
     }
 
     protected static boolean getForceRefresh() {
