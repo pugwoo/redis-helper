56b8145ba74fbafaa41e5244abf326a1069527f9
pugwoo <pugwoo@gmail.com>
2026-01-05 23:36:40 +0800

    单元测试

--- redis-helper-test-common/src/main/java/com/pugwoo/redishelpertest/common/TestRedisRateLimit.java
+++ redis-helper-test-common/src/main/java/com/pugwoo/redishelpertest/common/TestRedisRateLimit.java
@@ -17,8 +17,8 @@ public abstract class TestRedisRateLimit {
 
     @Test
     public void testRateLimit() {
-        // 起10个线程，连续调用3分20秒，非收尾时间段肯定是完整的1分钟，此时调用成功的数量应该是10000个
-        long endTime = System.currentTimeMillis() + 3 * 60 * 1000 + 20 * 1000;
+        // 起10个线程，连续调用35秒，非收尾时间段肯定是完整的10秒，此时调用成功的数量应该是1000个
+        long endTime = System.currentTimeMillis() + 35 * 1000;
         List<Thread> threads = new ArrayList<>();
         Map<String, AtomicLong> timeCount = new HashMap<>();
         for (int i = 0; i < 10; i++) {
@@ -26,7 +26,8 @@ public abstract class TestRedisRateLimit {
                 while (System.currentTimeMillis() <= endTime) {
                     try {
                         int second = DateUtils.getSecond(new Date());
-                        if (second < 10 || second > 50) { // 为了避免本地和Redis的时间差，这里只在10-50秒之间调用
+                        int secondMod10 = second % 10;
+                        if (secondMod10 < 2 || secondMod10 > 8) { // 为了避免本地和Redis的时间差，这里只在每10秒的2-8秒之间调用
                             continue;
                         }
 
@@ -35,11 +36,12 @@ public abstract class TestRedisRateLimit {
                         assert uuid.equals(uuid2);
 
                         synchronized (timeCount) {
-                            String minute = DateUtils.format(new Date(), "yyyy-MM-dd HH:mm");
-                            AtomicLong atomicLong = timeCount.get(minute);
+                            String tenSecond = DateUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+                            tenSecond = tenSecond.substring(0, tenSecond.length() - 1) + "0";
+                            AtomicLong atomicLong = timeCount.get(tenSecond);
                             if (atomicLong == null) {
                                 atomicLong = new AtomicLong();
-                                timeCount.put(minute, atomicLong);
+                                timeCount.put(tenSecond, atomicLong);
                             }
                             atomicLong.incrementAndGet();
                         }
@@ -63,12 +65,12 @@ public abstract class TestRedisRateLimit {
 
         System.out.println(JSON.toJson(timeCount));
 
-        // 不是开始和结束的时间段，应该是20000个
+        // 不是开始和结束的时间段，应该是1000个
         List<Map<String, Object>> list = ListUtils.transform(timeCount.entrySet(), o -> MapUtils.of("key", o.getKey(), "value", o.getValue()));
         ListUtils.sortAscNullLast(list, o -> (String) o.get("key"));
         boolean atLeastCheckOne = false;
         for (int i = 1; i < list.size() - 1; i++) {
-            assert list.get(i).get("value").toString().equals(String.valueOf(10000L));
+            assert list.get(i).get("value").toString().equals(String.valueOf(1000L));
             atLeastCheckOne = true;
         }
         assert atLeastCheckOne;
